33fa029bb5534632870ecd1d31cd51d5faf3e389
ignacioarroyo7 <ignacioarroyo17@gmail.com>
2022-03-19 17:21:08 -0300

    se crearon las rutas, se utilizo postman para ver las consultasy se importaron los middlewares correspondientes para ver en la terminal cuando realizan consultas, para recibir peticiones remotas y para recibir objetos en formato json

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 //creo una instancia de express
 const app = express();
@@ -9,4 +11,20 @@ app.set('port',process.env.PORT || 4000);
 
 app.listen(app.get('port'),()=>{
     console.log('estoy en el puerto'+app.get('port'));
+})
+
+//midlewares o configuraciones extras
+app.use(morgan('dev')) //parametro dev da info extra a la terminal
+app.use(cors());//permite recibir peticiones remotas
+//para recibir objetos en formato json
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//crear rutas
+app.get('/',(req,res)=>{
+    res.send('hola desde el servidor backend')
+})
+
+app.delete('/borrardatos',(req,res)=>{
+    res.send('aqui vamos a borrar')
 })
